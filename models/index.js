005c745c1c2713b12cd8bbc9a9c88e06695a8cc2
margaiwangara <margaiwangara@gmail.com>
2019-08-06 14:30:35 +0300

    fourth  mongURI test

--- models/index.js
+++ models/index.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 const config = require("config");
-const db = process.env.MONGO_URI.toString() | config.get("mongoURI");
+const db = process.env.MONGO_URI | config.get("mongoURI");
 const debug = process.env.MONGO_DEBUG | true;
+
+console.log("MONGO_URI" + process.env.MONGO_URI);
 
 mongoose.set("debug", true);
 mongoose.Promise = Promise;

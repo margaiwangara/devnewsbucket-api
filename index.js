8aaa5039837273d24c397ab265ad7b620740b20f
margaiwangara <margaiwangara@gmail.com>
2019-08-11 23:01:27 +0300

    testing with heroku

--- index.js
+++ index.js
@@ -66,12 +66,12 @@ let difference = nextHour - now;
 const url = "https://devnewsbucket.herokuapp.com/api";
 
 // delete db data
-// db.Article.deleteMany({})
-//   .then(resp => console.log("articles deleted"))
-//   .catch(error => console.log(error));
-// db.Request.deleteMany({})
-//   .then(resp => console.log("request deleted"))
-//   .catch(error => console.log(error));
+db.Article.deleteMany({})
+  .then(resp => console.log("articles deleted"))
+  .catch(error => console.log(error));
+db.Request.deleteMany({})
+  .then(resp => console.log("request deleted"))
+  .catch(error => console.log(error));
 setInterval(function() {
   axios
     .post(`${url}/articles`)
@@ -84,7 +84,7 @@ setInterval(function() {
     });
 
   console.log("Requesting...");
-}, 1000 * 60 * 60);
+}, 1000 * 60);
 
 // port
 const PORT = process.env.PORT || 5000;

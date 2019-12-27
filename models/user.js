75b52aa2b3564925a8d4a66ad5402e6e9f44f775
margaiwangara <margaiwangara@gmail.com>
2019-12-27 14:16:46 +0300

    added a config.env.example file for config examples, modified gitignore to include config example file, modified user model to include additional columns

--- models/user.js
+++ models/user.js
@@ -1,21 +1,54 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    maxlength: 100
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name field is required"],
+      maxlength: [255, "You have exceeded the maximum name length[255]"]
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: [true, "Email field is required"],
+      match: [
+        /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/,
+        "Please enter a valid email"
+      ],
+      maxlength: [100, "You have exceeded the maximum email length[100]"]
+    },
+    password: {
+      type: String,
+      minlength: [6, "Password length should be at least 6 characters"],
+      match: [
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/,
+        "Please enter a valid password, at least one lowercase and uppercase letter and one number"
+      ],
+      select: false,
+      required: [true, "Password field is required"]
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+      required: [true, "Role field is required"]
+    },
+    profileImage: {
+      type: String,
+      default: "no-image.jpg",
+      maxlength: [255, "You have exceeded the image name length[255]"]
+    },
+    resetPasswordToken: String,
+    passwordTokenExpire: Date,
+    confirmEmailToken: String,
+    isEmailConfirmed: {
+      type: Boolean,
+      default: false
+    }
   },
-  email: {
-    type: String,
-    maxlength: 100,
-    unique: true
-  },
-  password: {
-    type: String
-  }
-});
+  { timestamps: true }
+);
 
 // hash password before storing data
 userSchema.pre("save", async function(next) {

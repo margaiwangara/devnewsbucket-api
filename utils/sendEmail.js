ff8ef944615489390f9a312e7940cbb5ab67f498
margaiwangara <margaiwangara@gmail.com>
2020-09-07 12:19:44 +0300

    package upgrade, added sendEmail

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,21 +1,36 @@
 const nodemailer = require("nodemailer");
 const dotenv = require("dotenv");
-
+const MailGun = require('mailgun-js');
 // dotenv config
 dotenv.config({ path: `${__dirname}/../config/config.env` });
 
 // send email function
 const sendEmail = async options => {
+  const { SMTP_HOST, SMTP_PORT, SMTP_USERNAME, SMTP_PASSWORD, MAILGUN_API_KEY, MAILGUN_DOMAIN} = process.env;
+
   try {
     // create transporter
     const transporter = nodemailer.createTransport({
-      host: process.env.SMTP_HOST,
-      port: process.env.SMTP_PORT,
+      host: SMTP_HOST,
+      port: SMTP_PORT,
       secure: false,
       auth: {
-        user: process.env.SMTP_USERNAME,
-        pass: process.env.SMTP_PASSWORD
+        user: SMTP_USERNAME,
+        pass: SMTP_PASSWORD
       }
+    });
+
+    const mailgun = new MailGun({apiKey: MAILGUN_API_KEY, domain: MAILGUN_DOMAIN});
+    
+    mailgun.messages().send(options, function (err, body) {
+        //If there is an error, render the error page
+        if (err) {
+            console.log("Error: ", err);
+        }
+        //Else we can greet    and leave
+        else {
+            console.log('Message Sent', body);
+        }
     });
 
     // email information spread
